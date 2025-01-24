6924fe0c6052caffcf5ccd7b86bbcdbd655ded21
Rohitjadhav70 <rohitjadhav20181920@gmail.com>
2025-01-24 12:11:45 +0530

    added file

---
+++ fooddata.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="fooddata.aspx.cs" Inherits="fooddatadisplay.fooddata" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>  food quality check </title>
+    <style type="text/css">
+     
+        .auto-style1 {
+            width: 1268px;
+            height: 524px;
+        }
+     
+    </style>
+</head>
+<body>
+
+    <form id="form1" runat="server">
+             <div class="auto-style2">
+            <img alt="bagroundimage" class="auto-style1" src="pic/food_machine_background.jpg" />
+               <p align="center">
+                hwgwddiqdqwdqgdyajhjhqadqwgwducj  qwwdqwvwddhvdhjhjasjdjhvhdvqywuwddjhjhascjhsvahccvyuvcjcvcjhasvccygqvj
+                ddvqwvdvjjacabavcjhcvj  cb bccasvcvcvcacjasc
+                acvghavyvajhcvsajhhccggysacgavcchvac
+                cashcgygacycgjascvhcasjhcvhasfcascvcvhasvcavcvjhcvcac
+                cbhjacgyuacjvacavcasca
+                acbasyccyuacacbajshcashcciauchascasjhvchaasghgaacjahcbchascjhsachcf
+            </p>
+            <table align="center" class="auto-style2">
+
+                <tr>
+                    <td align="center">pH </td>
+                    <td align="center"> 
+                        <asp:TextBox ID="txtph" runat="server" Height="45px"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td align="center">Temperature</td>
+                    <td align="center">
+                        <asp:TextBox ID="txttemp" runat="server" Height="45px"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td align="center">Gas </td>
+                    <td align="center">
+                        <asp:TextBox ID="txtgas" runat="server" Height="45px"></asp:TextBox></td>
+                </tr>
+                 <tr>
+                    <td align="center">Turbidity  </td>
+                    <td align="center">
+                        <asp:TextBox ID="txtturbidity" runat="server" Height="45px"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td align="center">
+                        <asp:Button ID="btnsub" runat="server" Text="Submit" OnClick="btnsub_Click" style="height: 35px" /></td>
+                </tr>
+             
+                </table>
+
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ fooddata.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace fooddatadisplay
+{
+    public partial class fooddata : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Optionally load previously saved data
+                txtph.Text = Session["ph"] != null ? Session["ph"].ToString() : "";
+                txttemp.Text = Session["temp"] != null ? Session["temp"].ToString() : "";
+                txtgas.Text = Session["gas"] != null ? Session["gas"].ToString() : "";
+                txtturbidity.Text = Session["turbidity"] != null ? Session["turbidity"].ToString() : "";
+            }
+        }
+
+        protected void btnsub_Click(object sender, EventArgs e)
+        {
+            // Connection string (ensure it is correctly formatted)
+            string connectionString = @"Data Source=DESKTOP-3ATPPP8\SQLEXPRESS2012;Initial Catalog=foodsample;Persist Security Info=True;User ID=sa;Password=^lLoyyd567@";
+
+            // Define your SQL query with parameters
+            string query = @"INSERT INTO [dbo].[fooddata]
+                 ([ph], [temperature], [gas], [Turbidity])
+                 VALUES (@ph, @temperature, @gas, @turbidity)";
+
+            // Create and open the connection
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    // Add parameters with type conversion where necessary
+                    cmd.Parameters.AddWithValue("@ph", txtph.Text); // Assuming ph is varchar or string in the database
+                    cmd.Parameters.AddWithValue("@temperature", Convert.ToInt32(txttemp.Text)); // Convert string to int
+                    cmd.Parameters.AddWithValue("@gas", Convert.ToInt32(txtgas.Text));         // Convert string to int
+                    cmd.Parameters.AddWithValue("@turbidity", Convert.ToInt32(txtturbidity.Text)); // Convert string to int
+
+                    // Open connection
+                    con.Open();
+
+                    // Execute query
+                    cmd.ExecuteNonQuery();
+
+                    // Close connection
+                    con.Close();
+                }
+            }
+
+            // Inform the user
+            Response.Write("<script>alert('Data inserted successfully.');</script>");
+        }
+    }
+}
+
+
+    
